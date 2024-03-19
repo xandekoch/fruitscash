9a773af6761533006fc1cd7d16c3394104a1f432
Alexandre Koch <xandekoch.work@gmail.com>
2024-03-19 18:57:01 -0300

    canva responsive size added

--- src/_root/Game.tsx
+++ src/_root/Game.tsx
@@ -14,24 +14,30 @@ function Game() {
   );
 
   useEffect(() => {
-    const handleOrientationChange = () => {
+    const handleResize = () => {
       setIsLandscape(window.innerWidth > window.innerHeight);
     };
 
-    window.addEventListener("orientationchange", handleOrientationChange);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener("orientationchange", handleOrientationChange);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  useEffect(() => {
+    console.log("isLandscape:", isLandscape);
+  }, [isLandscape]);
+
+  const aspectRatio = 9 / 16; // Proporção desejada
 
   return (
     <Unity
       unityProvider={unityProvider}
       className="unity-canvas"
       style={{
-        height: isLandscape ? "100%" : "auto", // Altura 100% na orientação paisagem
-        width: isLandscape ? "auto" : "100%", // Largura 100% na orientação retrato
+        width: isLandscape ? `${100 / aspectRatio}vh` : "100%", // Largura proporcional na orientação paisagem, largura 100% na orientação retrato
+        height: isLandscape ? "100%" : `${aspectRatio * 100}vw`, // Altura 100% na orientação paisagem, altura proporcional na orientação retrato
         objectFit: "contain" // Manter a proporção de 16:9
       }}
     />
