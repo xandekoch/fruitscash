53aece69eea6e0fb66d0187be8f575d3ab8913e6
Alexandre Koch <xandekoch.work@gmail.com>
2024-03-30 20:39:38 -0300

    guard refresh devtools rmv

--- src/lib/guard.ts
+++ src/lib/guard.ts
@@ -29,26 +29,26 @@ document.addEventListener('contextmenu', function (event) {
   event.preventDefault();
 });
 
-// Ficar dando refresh automatico na página se o desenvolvedor estiver aberto
-let devtoolsOpen = false;
+// // Ficar dando refresh automatico na página se o desenvolvedor estiver aberto
+// let devtoolsOpen = false;
 
-// Verificar a largura do console do desenvolvedor
-const checkDevtools = () => {
-  const widthThreshold = window.outerWidth - window.innerWidth > 160;
-  const heightThreshold = window.outerHeight - window.innerHeight > 160;
-  devtoolsOpen = widthThreshold || heightThreshold;
-};
+// // Verificar a largura do console do desenvolvedor
+// const checkDevtools = () => {
+//   const widthThreshold = window.outerWidth - window.innerWidth > 160;
+//   const heightThreshold = window.outerHeight - window.innerHeight > 160;
+//   devtoolsOpen = widthThreshold || heightThreshold;
+// };
 
-// Atualizar a variável devtoolsOpen
-checkDevtools();
+// // Atualizar a variável devtoolsOpen
+// checkDevtools();
 
-// Verificar periodicamente se o console do desenvolvedor está aberto
-setInterval(() => {
-  checkDevtools();
-  if (devtoolsOpen) {
-    console.log('Console do desenvolvedor está aberto!');
-    window.location.reload();
-  }
-}, 1000);
+// // Verificar periodicamente se o console do desenvolvedor está aberto
+// setInterval(() => {
+//   checkDevtools();
+//   if (devtoolsOpen) {
+//     console.log('Console do desenvolvedor está aberto!');
+//     window.location.reload();
+//   }
+// }, 1000);
 
 export {};
